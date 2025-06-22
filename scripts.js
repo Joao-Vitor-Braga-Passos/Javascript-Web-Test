87c84aeb73cafd0752e58297515bfbabf484ff24
Joao-Vitor-Braga-Passos <bragapassosjoao25@gmail.com>
2025-06-22 14:50:47 -0300

    Euro value added

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 const convertButton = document.querySelector(".convert-button")
+const currencySelector = document.querySelector(".currency-select")
 
 function convertValues(){
     console.log("funcionou")
@@ -6,14 +7,34 @@ function convertValues(){
     const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
     const currencyValueConverted = document.querySelector(".currency-value")
 
+    console.log(currencySelector.value)
     const dolarToday = 5.2
+    const euroToday = 6.2
 
-    const convertValue = inputCurrencyValue / dolarToday
+    if(currencySelector.value == "dolar"){
+        //Se o select estiver selecionado o valor de dolar,entre aqui
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD"
+    }).format(inputCurrencyValue / dolarToday)
 
-    currencyValueToConvert.innerHTML = inputCurrencyValue
+    }
+    if(currencySelector.value == "euro"){ 
+        //Se o select estiver selecionado o valor de euro,entre aqui
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-UK", {
+        style: "currency",
+        currency: "EUR"
+    }).format(inputCurrencyValue / euroToday)
+
+    }
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue)
+
 
     console.log(inputCurrencyValue)
-    console.log(convertValue)
 }
 
 convertButton.addEventListener("click", convertValues)
